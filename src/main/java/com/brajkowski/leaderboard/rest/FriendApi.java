93a33ec2b5fe7a09d755427d07426b467d4380eb
brandon <brandon@ubuntu>
2019-04-13 18:35:06 -0700

    code cleanup

--- src/main/java/com/brajkowski/leaderboard/rest/FriendApi.java
+++ src/main/java/com/brajkowski/leaderboard/rest/FriendApi.java
@@ -4,13 +4,9 @@ import com.brajkowski.leaderboard.service.FriendService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
