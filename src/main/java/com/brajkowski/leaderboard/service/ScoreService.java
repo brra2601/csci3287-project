3b958129e3a49341d081e162d2d2c75749791731
brandon <brandon@ubuntu>
2019-04-21 17:12:18 -0700

    implemented getting high scores by user filter and view filter separately

--- src/main/java/com/brajkowski/leaderboard/service/ScoreService.java
+++ src/main/java/com/brajkowski/leaderboard/service/ScoreService.java
@@ -1,5 +1,13 @@
 package com.brajkowski.leaderboard.service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAmount;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import com.brajkowski.leaderboard.dao.ScoreDao;
@@ -18,19 +26,59 @@ public class ScoreService {
     private FriendService friendService;
 
     public List<Score> getFilteredHighScores(int levelId, String userFilter, String viewFilter, String username) {
-        userFilter = userFilter.toLowerCase();
-        if (userFilter.compareTo("me") == 0) {
-            return scores.getHighScoresByLevelUserlist(levelId, List.of(username));
+        if (userFilter != null && viewFilter != null) {
+            return null; // TODO: return scores based on view and user filters
         }
-        else if (userFilter.compareTo("friends") == 0) {
-            UsernameList friendList = friendService.generateFriendList(username);
-            return scores.getHighScoresByLevelUserlist(levelId, friendList.usernames);
+        if (userFilter != null) {
+            return scores.getHighScoresByLevelUserlist(levelId, generateUserFilter(userFilter, username));
         }
-        // TODO: return global scores.
-        return null;
+        if (viewFilter != null) {
+            return scores.getHighScoresByLevelAfterDatetime(levelId, generateViewFilter(viewFilter));
+        }
+        return scores.getHighScoresByLevel(levelId);
     }
 
     public List<Score> getFilteredHighScores(int levelId, String userFilter, String viewFilter, String username, int limit) {
         return null;
+    }
+
+    private static Timestamp generateDayAgo() {
+        return Timestamp.from(ZonedDateTime.now().minusDays(1).toInstant());
+    }
+
+    private static Timestamp generateMonthAgo() {
+        return Timestamp.from(ZonedDateTime.now().minusMonths(1).toInstant());
+    }
+
+    private static Timestamp generateYearAgo() {
+        return Timestamp.from(ZonedDateTime.now().minusYears(1).toInstant());
+    }
+
+    private List<String> generateUserFilter(String userFilter, String username) {
+        List<String> usernames;
+        switch (userFilter.toLowerCase()) {
+            case "me":
+                usernames = List.of(username);
+                break;
+            case "friends":
+                usernames = friendService.generateFriendList(username).usernames;
+                break;
+            default:
+                return new ArrayList<String>();
+        }
+        return usernames;
+    }
+
+    private static String generateViewFilter(String viewFilter) {
+        switch (viewFilter.toLowerCase()) {
+            case "day":
+                return generateDayAgo().toString();
+            case "month":
+                return generateMonthAgo().toString();
+            case "year":
+                return generateYearAgo().toString();
+            default:
+                return Timestamp.from(Instant.now()).toString();
+        }
     }
 }
